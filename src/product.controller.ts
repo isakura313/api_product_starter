343bfa0fb8f139f8b804880678649f9f85ead09c
Pavel Yakupov <isakura313@gmail.com>
2024-05-12 21:30:04 +0300

    fix: initial commit

---
+++ src/product.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ProductService } from './product.service';
+import { Product } from './interfaces/product.interface';
+
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  async create(@Res() response, @Body() createProductDto: CreateProductDto) {
+    console.log(createProductDto);
+    try {
+      const newProduct = this.productService.create(createProductDto);
+      return response.status(HttpStatus.CREATED).json({
+        message: 'Product has been created successfully',
+        newProduct,
+      });
+    } catch (err) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: 400,
+        message: 'Error: Quest not created!',
+        error: 'Bad Request',
+      });
+    }
+  }
+
+  @Get()
+  async findAll(): Promise<Product[]> {
+    return this.productService.findAll();
+  }
+}
